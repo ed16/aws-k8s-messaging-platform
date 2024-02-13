9383d246883e1e7be8dc69c58840caf75529d74f
ed16 <e.zaynullin@gmail.com>
2024-02-13 14:21:33 +0100

    Add pprof

--- services/user-service/cmd/main.go
+++ services/user-service/cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/ed16/aws-k8s-messaging-platform/services/user-service/pkg/api"
 )
 
 func main() {
 	api.HandleRequests()
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
